8b16e8c289324a9c1a4fd29ed77beeb9e2696f87
akacronos <jorge.escamilla89@gmail.com>
2016-02-09 03:18:00 -0600

    Add styles for headers in sections

--- gulpfile.js
+++ gulpfile.js
@@ -79,7 +79,7 @@ gulp.task('styles', function(){
 gulp.task('watch', function(){
   livereload.listen();
 
-  gulp.watch('./*.html', ['html']);
+  gulp.watch('./*/*.html', ['html']);
   gulp.watch('scripts/*.js', ['scripts']);
   gulp.watch('css/stylus/*/*.styl', ['styles']);
 });
