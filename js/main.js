a4f7921a60fdd7ef27a14fa1ef6ab1509fc253bf
akacronos <jorge.escamilla89@gmail.com>
2016-02-10 03:17:38 -0600

    Add slider in sections

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.slider').unslider({
+    autoplay: true,
+    infinite: true,
+    nav: false,
+    arrows: {
+      prev: '<a class="unslider-arrow prev"><img src="/img/arrow-left.png" alt="" /></a>',
+      next: '<a class="unslider-arrow next"><img src="/img/arrow-right.png" alt="" /></a>'
+    }
+  });
+});
